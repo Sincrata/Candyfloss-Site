09908f11633315d6a16cf7e9f21bec841aabfd7a
Sincrata <catgal25@hotmail.com>
2026-04-28 23:17:16 -0400

    Homepage fix

---
+++ components/clangems-component.js
@@ -0,0 +1,33 @@
+class ClanGems extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+
+<div class="row" style="flex-wrap: wrap;width:100%;display:flex;justify-content:center;padding:25px;">
+	<a href="https://toyhou.se/~world/53663.the-candyfloss/page/10531.kappa-clan"><img class="clangem" src="gems/kappa.png" style="margin-right:5px;"></a>
+	<a href="https://toyhou.se/~world/53663.the-candyfloss/page/27140.omega" ><img class="clangem" src="gems/omega.png" style="margin-right:5px;"></a>
+	<a href="https://toyhou.se/~world/53663.the-candyfloss/page/10528.mu-clan" ><img class="clangem" src="gems/mu.png" style="margin-right:5px;"></a>
+	<a href="https://toyhou.se/~world/53663.the-candyfloss/page/10532.psi-clan" ><img class="clangem" src="gems/psi.png" style="margin-right:5px;"></a>
+	<a href="https://toyhou.se/~world/53663.the-candyfloss/page/120070.eta" ><img class="clangem" src="gems/eta.png" style="margin-right:5px;"></a>
+	<a href="https://toyhou.se/~world/53663.the-candyfloss/page/10520.alpha-clan" ><img class="clangem" src="gems/alpha.png" style="margin-right:5px;"></a>
+	<a href="https://toyhou.se/~world/53663.the-candyfloss/page/10521.gamma-clan" ><img class="clangem" src="gems/gamma.png" style="margin-right:5px;"></a>
+	<a href="https://toyhou.se/~world/53663.the-candyfloss/page/10525.delta-clan" ><img class="clangem" src="gems/delta.png" style="margin-right:5px;"></a>
+	<a href="https://toyhou.se/~world/53663.the-candyfloss/page/10529.nu-clan" ><img class="clangem" src="gems/nu.png" style="margin-right:5px;"></a>
+	<a href="https://toyhou.se/~world/53663.the-candyfloss/page/70294.chi-clan" ><img class="clangem" src="gems/chi.png" style="margin-right:5px;"></a>
+	<a href="https://toyhou.se/~world/53663.the-candyfloss/page/10522.beta-clan" ><img class="clangem" src="gems/beta.png" style="margin-right:5px;"></a>
+	<a href="https://toyhou.se/~world/53663.the-candyfloss/page/10530.phi-clan" ><img class="clangem" src="gems/phi.png" style="margin-right:5px;"></a>
+	<a href="https://toyhou.se/~world/53663.the-candyfloss/page/10526.zeta-clan" ><img class="clangem" src="gems/zeta.png" style="margin-right:5px;"></a>
+	<a href="https://toyhou.se/~world/53663.the-candyfloss/page/10523.theta-clan" ><img class="clangem" src="gems/theta.png" style="margin-right:5px;"></a>
+	<a href="https://toyhou.se/~world/53663.the-candyfloss/page/10524.epsilon-clan" ><img class="clangem" src="gems/epsilon.png" style="margin-right:5px;"></a>
+	<a href="https://toyhou.se/~world/53663.the-candyfloss/page/11451.rho-clan" ><img class="clangem" src="gems/rho.png" style="margin-right:5px;"></a>
+	<a href="https://toyhou.se/~world/53663.the-candyfloss/page/10589.lambda-clan" ><img class="clangem" src="gems/lambda.png" style="margin-right:5px;"></a>
+	<a href="https://toyhou.se/~world/53663.the-candyfloss/page/47752.sigma-clan" ><img class="clangem" src="gems/sigma.png" style="margin-right:5px;"></a>
+	<a href="https://toyhou.se/~world/53663.the-candyfloss/page/128717.xxxxxxxxxxxomi" ><img class="clangem" src="gems/omicron.png" style="margin-right:5px;"></a>
+	<a href="https://toyhou.se/~world/53663.the-candyfloss/page/160622.eataf-clan" ><img class="clangem" src="gems/eataf.png" style="margin-right:5px;"></a>
+	<a href="https://toyhou.se/~world/53663.the-candyfloss/page/10527.iota-clan" ><img class="clangem" src="gems/iota.png" style="margin-right:5px;"></a>
+	<a href="https://toyhou.se/~world/53663.the-candyfloss/page/181918.miden-clan" ><img class="clangem" src="gems/miden.png" style="margin-right:5px;"></a>
+</div>
+
+`;
+  }
+}
+customElements.define('clan-gems', ClanGems);
