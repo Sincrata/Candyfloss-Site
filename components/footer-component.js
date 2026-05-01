34b6d4ae5c67aa76bc27013773fae4d3a9fe13c0
Sincrata <catgal25@hotmail.com>
2026-04-30 22:34:08 -0400

    Prep

--- components/footer-component.js
+++ components/footer-component.js
@@ -5,7 +5,7 @@ class Footer extends HTMLElement {
 		Tenshilove Studios LLC<br>&#128712; 
 		<a href="https://toyhou.se/~world/53663.the-candyfloss/page/10511.what-is-candyfloss">ABOUT</a> 
 		| &#128464; <a href="https://toyhou.se/~world/53663.the-candyfloss/page/10514.species-and-group-rules">TOS</a> 
-		| &#128401; <a href="#">STAFF</a> 
+		| &#128401; <a href="staffinformation.html">STAFF</a> 
 		| &#128490; <a href="https://toyhou.se/~world/53663.the-candyfloss/page/88594.candyfloss-discord">DISCORD</a> 
 		| &#8962;<a href="https://toyhou.se/~world/53663.the-candyfloss">TOYHOU.SE
 	</center>
